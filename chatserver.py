9ca866c94ff4f86be2c1306ad844d66b2cf9e7ec
kireetipatamsetti <paki17@student.bth.se>
2020-05-31 18:24:38 +0200

    Add broadcating module

--- chatserver.py
+++ chatserver.py
@@ -1,7 +1,7 @@
-
 import sys
 import re
 import socket
+
 if len(sys.argv) != 2:
     print("Usage: ./chatserver <ip>:<port>")
     sys.exit(1)
@@ -16,3 +16,17 @@ server.listen(100)
 clients = [server]
 clients_in_queue = []
 permanent_clients = {}
+
+
+def broadcast(message, client):
+    for socket in permanent_clients.keys():
+        if socket != client:
+            try:
+                print("Broadcasting!")
+                socket.sendall(message.encode('ascii'))
+            except:
+                socket.close()
+                del permanent_clients[socket]
+                clients.remove(socket)
+
+
