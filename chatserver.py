4285eb5d334e1ce703361ad648c7e929a664d483
kireetipatamsetti <paki17@student.bth.se>
2020-05-31 18:23:47 +0200

    Add code to socket connection

--- chatserver.py
+++ chatserver.py
@@ -9,3 +9,10 @@ args = str(sys.argv[1]).split(':')
 ip = str(args[0])
 port = int(args[1])
 
+server = socket.socket()
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((ip, port))
+server.listen(100)
+clients = [server]
+clients_in_queue = []
+permanent_clients = {}
